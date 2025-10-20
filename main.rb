7c346ce420a70804245c6f1e80fad2d4493439f7
moepyxxx <satoyukashi@gmail.com>
2025-10-20 23:29:29 +0900

    update: ポケモンクラスを呼び出せるようにした

--- main.rb
+++ main.rb
@@ -1,8 +1,13 @@
 #!/usr/bin/env ruby
 require 'bundler/setup'
 require 'tty-prompt'
+require_relative 'pokemon/pokemon'
 
 prompt = TTY::Prompt.new
 
-choice = prompt.select("何をしますか？", %w(起動 停止 再起動 終了))
-puts "#{choice}を実行します"
+# choice = prompt.select("何をしますか？", %w(起動 停止 再起動 終了))
+# puts "#{choice}を実行します"
+
+pikachu = Pokemon.new("ピカチュウ", "でんき", 100)
+prompt.say("ポケモンが登場した！")
+prompt.say(pikachu.status)
---
+++ pokemon/pokemon.rb
@@ -0,0 +1,19 @@
+class Pokemon 
+  def initialize(name, type, max_hp)
+    @name = name
+    @type = type
+    @hp = max_hp
+  end
+
+  def is_alive?
+    return @hp > 0
+  end
+
+  def status
+    return "#{@name} (Type: #{@type}) - HP: #{@hp}"
+  end
+
+  def take_damage(damage)
+    @hp -= damage
+  end
+end
